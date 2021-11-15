68d50a612d4f8cec34a0a7fc590e7f9a351df335
PopBang <popbang@mail.com>
2021-11-15 11:35:44 +0000

    Initial commit from gatsby: (https://github.com/PopBang/starter.git)

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `pop-bang-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+
+        icon: `src/images/gatsby-icon.png`,
+
+        // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
